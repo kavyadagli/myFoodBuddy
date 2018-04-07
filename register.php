512458297ae4466c1c0e0fe64460d6b8705b5c56
Ashish Bachavala <ashish0810@yahoo.com>
2018-04-07 10:00:08 -0700

    Made reg php func

--- register.php
+++ register.php
@@ -1,6 +1,16 @@
 <?php
 
 session_start();
+
+$user = $_POST['user'];
+$pass = $_POST['pass'];
+$age = $_POST['age'];
+$height = $_POST['height'];
+$weight = $_POST['weight'];
+$gender = $_POST['gender'];
+$activity = $_POST['activity'];
+
+
 $servername = "localhost";
 $username = "root";
 $password = "ashSQLpassword";
@@ -12,6 +22,14 @@ if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 }
 
+$sql = "insert into login (user, pass, name, gender, age, height, weight, activity) values ('$user', '$pass', '$name', '$gender', '$age', '$height', '$weight', '$activity');";
 
+if ($conn->query($sql) === TRUE) {
+	$_SESSION['user'] = $user;
+	$_SESSION['name'] = $name;
+	header('Location: dash.php');
+} else {
+	echo "Had some problems, please return to registration form and try again";
+}
 
 ?>
