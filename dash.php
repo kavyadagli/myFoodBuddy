4810bd27e3506d1bc8b8c5818c46f60baccedb3e
Ashish Bachavala <ashish0810@yahoo.com>
2018-04-07 11:32:21 -0700

    Linking Style

---
+++ dash.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Food Buddy - Dashboard</title>
+	<link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+	<?php
+	session_start();
+
+	?>
+	<
