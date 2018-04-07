b1bafaf821d898242e01ea5eb7df2550d2e23413
Ashish Bachavala <ashish0810@yahoo.com>
2018-04-07 10:29:58 -0700

    registration

--- register.php
+++ register.php
@@ -9,32 +9,32 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
-<body>
+<body onload="setInterval(checkFormValid(), 500)">
 
 	<h1>Register for My Food Buddy</h1>
 
 	<form alight="center" id="form-ajax" action="reg_sup.php" method="post">
-		<label alight="center">Username</label><br/><input type="text" name="user" /><br /><br />
+		<label alight="center">Username</label><br/><input type="text" name="user" id="user" /><br /><br />
 
-		<label>Password</label><br/><input type="text" name="password" /><br /><br />
+		<label>Password</label><br/><input type="text" name="pass" id="pass" /><br /><br />
 
-		<label>Repeat Password</label><br /><input type="password" name="pass2" /><br /><br />
+		<label>Repeat Password</label><br /><input type="password" name="pass2" id="pass" /><br /><br />
 
-		<label>Age</label><br/><input type="text" name="age" /><br /><br/>
+		<label>Age</label><br/><input type="number" name="age" id="age" /><br /><br/>
 
-		<label>Height</label><br/><input type="text" name="height"/><label> inches</label><br/><br/>
+		<label>Height</label><br/><input type="number" name="height" id="height" /><label> inches</label><br/><br/>
 
-		<label>Weight</label><br/><input type="text" name="weight" /><label> lbs.</label><br /><br/>
+		<label>Weight</label><br/><input type="number" name="weight" id="weight" /><label> lbs.</label><br /><br/>
 
 		<div class="dropdown">
-			<select name="gender">
+			<select name="gender" id="gender">
 	  			<option value="F">Female</option>
 	  			<option value="M">Male</option>
 			</select>
 		</div><br/>
 
 		<div class="dropdown">
-			<select name="activityLevel">
+			<select name="activity" id="activity">
 	  			<option value="1.2">Sendentary</option>
 	  			<option value="1.375">Light Activity</option>
 	  			<option value="1.55">Moderate Activity</option>
@@ -43,9 +43,19 @@
 			</select>
 		</div><br/>
 
+		<script type="text/javascript">
+			function checkFormValid() {
+				if (document.getElementById("pass").value == document.getElementById("pass2")) {
+					document.getElementById("submit").disabled = false;
+					document.getElementById("errorText").innerHTML = "Passwords must match";
+				} else {
+					document.getElementById("submit").disabled = false;
+					document.getElementById("errorText").innerHTML = "";
+				}
+			}
+		</script>
 
-
-		<button type="button">Submit</button>
+		<button type="button" id="submit">Submit</button><p id="errorText"></p>
 	</form>
 
 </body>
