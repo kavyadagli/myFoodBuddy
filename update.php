043350ddcac478a6d9d1039e5c8287830bf642d7
Ashish Bachavala <ashish0810@yahoo.com>
2018-04-07 21:40:17 -0700

    a

--- update.php
+++ update.php
@@ -7,18 +7,14 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 $date = date("Y-m-d");
-$sql = "SELECT food, calories FROM $table WHERE eatdate='$date'";
+$sql = "SELECT food, calories FROM $table";// WHERE eatdate='$date'";
 $result = $conn->query($sql);
 
 echo "<div id='foodEaten' style='display: block;'>";
 if ($result->num_rows > 0) {
   echo "<table border='1' class='foodTab'><tr><th>Food</th><th>Calories</th></tr>";
   while($row = $result->fetch_assoc()) {
-    $arrDP = explode("-", $rowP["due"]);
-    $dayP = date("D", mktime(0, 0, 0, $arrDP[1], $arrDP[2], $arrDP[0]));
-    $dateP = $arrDP[1] . "/" .  $arrDP[2] . ", " . $dayP;
-    $linkDoneP = "finished.php?id=" . $rowP["id"]/* . "&user=$table"*/;
-    echo "<tr><td>" . $row["food"] . "</td><td>" . $rowP["calories"] . "</td></tr>";
+    echo "<tr><td>" . $row["food"] . "</td><td>" . $row["calories"] . "</td></tr>";
   }
   echo "</table>";
 } else {
