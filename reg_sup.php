3e1bd300f9f866d8fcb56ede63732991e1260310
Kavya <kdagli@terpmail.umd.edu>
2018-06-24 18:17:55 -0400

    Update reg_sup.php

--- reg_sup.php
+++ reg_sup.php
@@ -16,7 +16,7 @@ $username = "root";
 $password = "ashSQLpassword";
 $dbname = "foodDB";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli(/*insert information*/);
 
 if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
